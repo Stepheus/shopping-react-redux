29904736b1bb10dc7f9e4d12bf8392c2ba9da712
Stepheus <stephen.iboudah@outlook.com>
2026-01-09 22:50:49 -0500

    dispatch selector

--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -1,28 +1,32 @@
 import React from 'react';
 import './ProductList.css'; 
+import {useDispatch, useSelector} from "react-redux";
+import {addItemToCart } from "./CartSlice"; 
 
 const ProductList = () => {
+    const dispatch = useDispatch;
+    const cartItems = useSelector(state => state.cart.cartItems);
 
-  const products = [
+    const products = [
     { id: 1, name: 'Product A', price: 60 },
     { id: 2, name: 'Product B', price: 75 },
     { id: 3, name: 'Product C', price: 30 },
-  ];
+    ];
 
-  return (
+    return (
     <div className="product-list">
-      <h2 className="product-list-title">Products</h2>
-      <ul className="product-list-items">
+        <h2 className="product-list-title">Products</h2>
+        <ul className="product-list-items">
         {products.map(product=> (
             <li key={product.id} className ="product-list-item">
                 <span>{product.name}-${product.price}</span>
                 <button>Add to Cart</button>
             </li>
         ))};
-     
-      </ul>
+        
+        </ul>
     </div>
-  );
+    );
 };
 
 export default ProductList;
