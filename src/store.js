bbd8692e89f8610ffcb474dd1ee02fd68e467e1e
Stepheus <stephen.iboudah@outlook.com>
2026-01-10 20:26:59 -0500

    App complete

--- src/store.js
+++ src/store.js
@@ -1,10 +1,9 @@
-import {configureStore} from "@reduxjs/toolkit";
-import cartReducer from "./Components/CartSlice";
-
+import { configureStore } from '@reduxjs/toolkit';
+import cartReducer from './Components/CartSlice';
 const store = configureStore({
-    reducer: {
-        cart: cartReducer,
-    },
+  reducer: {
+    cart: cartReducer,
+  },
 });
 
 export default store;
